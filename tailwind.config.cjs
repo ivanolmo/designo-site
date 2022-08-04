5ca77eff11660b3b402c6fcf9856e0f770225c76
Ivan Olmo <ivan.olmo@gmail.com>
2022-08-04 17:39:02 -0500

    Add colors per design system

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: colors.white,
+      black: '#1d1c1e',
+      peach: {
+        400: '#ffad9b',
+        600: '#e7816b',
+      },
+      gray: {
+        200: '#f1f3f5',
+        800: '#333136',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
