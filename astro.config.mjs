142d0407ff689298bc4a6f4cbb17c26d082150eb
Ivan Olmo <ivan.olmo@gmail.com>
2022-08-25 11:27:49 -0500

    Add Vercel adapter and update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact({ compat: true })],
+  output: 'server',
+  adapter: vercel(),
 });
