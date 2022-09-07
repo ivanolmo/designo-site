1b7af0b8f50b77c05ea576c12148a189fb0e968a
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-07 18:56:42 -0500

    Add robots.txt package to Astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import vercel from '@astrojs/vercel/serverless';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact({ compat: true })],
+  site: 'https://designo-site-ivan.vercel.app/',
+  experimental: {
+    integrations: true,
+  },
+  integrations: [tailwind(), preact({ compat: true }), robotsTxt()],
   output: 'server',
   adapter: vercel(),
 });
